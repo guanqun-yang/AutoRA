e9a0ac9071a0ed090d8b1fe570eb5680648a5ab3
Guanqun Yang <guanqun.yang@outlook.com>
2024-09-19 22:55:18 -0400

    update

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
 # AutoRA: Unlocking Smarter Research with Automated Paper Search and Analysis
 
 ![](resources/003-logo.png)
+## Environment
+
+- The code was developed on a gaming laptop equipped with an RTX-2060 GPU (6GB VRAM) running Ubuntu 20.04. While the code should be adaptable to other hardware configurations, compatibility may vary.
+- Run the following to install the library:
+
+```bash
+conda create --name autora python==3.10
+pip install -e .
+python app.py
+```
 
 ## Features
 
-- [x] Fetch the complete conference proceedings from the DBLP and ACL Anthology.
-- [x] Perform semantic search based on your keywords of interest with the help of LLMs.
-- [x] Perform the semantic search from a Flask-based portal.
-- [x] Flag the interested papers with red background.
-- [ ] Improve the presentation of the Flask-based portal by better formatting the special characters.
-- [ ] Improve the user-friendliness of the system by automating everything except (1) creating the environment, and (2) `python app.py`.
-- [ ] Add (1) a selector to filter the papers based on years and venues, (2) a checker box or slider on how many papers to return.
-- [ ] Add a progress bar that tracks the browsing progress and use it start from what is left.
-- [ ] Maintain a history of previous queries.
+ - [x] Retrieve complete conference proceedings from DBLP and the ACL Anthology.
+ - [x] Conduct semantic searches using `sentence_transformers` embeddings based on your chosen keywords.
+ - [x] Access the semantic search through a Flask-based portal.
+ - [x] Highlight selected papers with a red background.
+ - [ ] Enhance the Flask portal's presentation by properly formatting special characters.
+ - [x] Increase system user-friendliness by automating all tasks except (1) environment setup and (2) running `python app.py`.
+ - [ ] Add features to filter papers by year and venue, and include a checkbox or slider to control the number of papers returned.
+ - [ ] Implement a progress bar that tracks browsing and resumes from where you left off.
+ - [ ] Maintain a history of previous search queries.
 
 
 ![](resources/004-demo.png)
--- app.py
+++ app.py
@@ -29,33 +29,15 @@ from setting import setting
 
 app = Flask(__name__)
 
-def search_papers(query):
+def search_nlp_papers(query, top_k=20):
     ##################################################
     # search NLP conferences
     ##################################################
+    EMBEDDING_FILE = setting.DATASET_PATH / "acl_embedding.pkl"
+    INDEX_FILE = setting.DATASET_PATH / "acl_index.bin"
 
-    # df = pd.read_pickle(setting.DATASET_PATH / "acl.pkl")
-    # df = df[df.year.isin(["2023", "2024"])]
-
-    # records = df.to_dict(orient="records")
-    #
-    # embeddings = load_pickle_file(setting.DATASET_PATH / "acl_embedding.pkl")
-    # index = create_index(embeddings, batch_size=32)
-    #
-    # cprint(f"SEARCH QUERY: {query}")
-    # queries = [entry.strip() for entry in query.split(",")]
-    #
-    # searched_df = search_index(queries, records, index, top_k=200)
-    # searched_papers = searched_df[["title", "url", "author", "abstract"]].fillna("NONE").to_dict(orient="records")
-
-    ##################################################
-    # search SE papers
-    ##################################################
-    EMBEDDING_FILE = setting.DATASET_PATH / "se_embedding.pkl"
-    INDEX_FILE = setting.DATASET_PATH / "se_index.bin"
-
-    df = pd.read_pickle(setting.DATASET_PATH / "se.pkl")
-    df = df[df.update_date >= "2022-01-01"]
+    df = pd.read_pickle(setting.DATASET_PATH / "acl.pkl")
+    df = df[df.year.isin(["2023", "2024"])]
 
     records = df.to_dict(orient="records")
 
@@ -63,7 +45,7 @@ def search_papers(query):
         index = faiss.read_index(str(INDEX_FILE))
     else:
         if EMBEDDING_FILE.exists():
-            embeddings = load_pickle_file(setting.DATASET_PATH / "se_embedding.pkl")
+            embeddings = load_pickle_file(EMBEDDING_FILE)
         else:
             embeddings = embed_documents(records, batch_size=32)
             save_pickle_file(embeddings, EMBEDDING_FILE)
@@ -77,8 +59,44 @@ def search_papers(query):
     cprint(f"SEARCH QUERY: {query}")
     queries = [entry.strip() for entry in query.split(",")]
 
-    searched_df = search_index(queries, records, index, top_k=200)
+    searched_df = search_index(queries, records, index, top_k=int(top_k))
+    searched_papers = searched_df[["title", "url", "author", "abstract"]].fillna("NONE").to_dict(orient="records")
+
+    return searched_papers
+
+def search_se_papers(query, top_k=20):
+    EMBEDDING_FILE = setting.DATASET_PATH / "se_embedding.pkl"
+    INDEX_FILE = setting.DATASET_PATH / "se_index.bin"
+
+    df = pd.read_pickle(setting.DATASET_PATH / "se.pkl")
+    df = df[df.update_date >= "2022-01-01"]
+
+    records = df.to_dict(orient="records")
+
+    if INDEX_FILE.exists():
+        index = faiss.read_index(str(INDEX_FILE))
+    else:
+        if EMBEDDING_FILE.exists():
+            embeddings = load_pickle_file(EMBEDDING_FILE)
+        else:
+            embeddings = embed_documents(records, batch_size=32)
+            save_pickle_file(embeddings, EMBEDDING_FILE)
+
+        index = create_index(embeddings, batch_size=32)
+        if torch.cuda.is_available():
+            index = faiss.index_gpu_to_cpu(index)
+
+        faiss.write_index(index, str(INDEX_FILE))
+
+    cprint(f"SEARCH QUERY: {query}")
+    queries = [entry.strip() for entry in query.split(",")]
+
+    searched_df = search_index(queries, records, index, top_k=int(top_k))
+    # add url
     searched_df["url"] = searched_df.apply(lambda x: "https://www.arxiv.org/abs/{}".format(x["id"]), axis="columns")
+    # add arXiv ID to the title
+    searched_df["title"] = searched_df.apply(lambda x: "[{}] {}".format(x["id"], x["title"]), axis="columns")
+    # rename the columns for consistency with the UI
     searched_df = searched_df.rename({"authors": "author"}, axis="columns")
 
     searched_papers = searched_df[["title", "url", "author", "abstract"]].fillna("NONE").to_dict(orient="records")
@@ -93,8 +111,15 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    queries = request.form['query']
-    papers = search_papers(queries)
+    queries = request.form.get('query')
+    num_papers = request.form.get('numPapers')
+    paper_type = request.form.get('paperType')
+
+    if paper_type == "NLP":
+        papers = search_nlp_papers(queries, top_k=num_papers)
+    else:
+        papers = search_se_papers(queries, top_k=num_papers)
+
     return jsonify(papers)
 
 
